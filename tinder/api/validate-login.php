16c419dae7562120a9afe10cae45b8071a83d233
CFarver <casperfarver@hotmail.com>
2018-03-08 22:48:39 +0100

    Login

---
+++ tinder/api/validate-login.php
@@ -0,0 +1,39 @@
+<?php
+//Validate the login information
+
+  $sFileName = '../txt/users.txt';
+
+  $sUsername = $_POST['username'];
+  $sPassword = $_POST['password'];
+
+  $sajUsers = file_get_contents($sFileName);
+  $ajUsers = json_decode($sajUsers);
+
+  //If the input fields are not empty - should not be possible - due to validation in frontend. But just to ensure.
+  if(!empty($sUsername) && !empty($sPassword)) {
+    
+    //Check username and password combo
+    for ($i=0; $i < count($ajUsers); $i++) { 
+      if($ajUsers[$i]->username == $sUsername && $ajUsers[$i]->password == $sPassword) {
+          
+          if($ajUsers[$i]->verification == true) {
+            echo '{"status" : "success", "message" : "Username and password match is found"}';
+            exit;
+          } else {
+            echo '{"status" : "pending", "message" : "Username and password is correct, but account needs to be activated"}';
+            exit;
+          }
+          
+      } 
+    }
+    echo '{"status" : "error", "message" : "Username and/or password do not match!"}';
+    exit;
+  }
+
+
+  
+
+
+
+
+?>
--- tinder/js/main.js
+++ tinder/js/main.js
@@ -1,10 +1,14 @@
 //Function that runs when document is loaded
 $(document).ready(function() {
+  
+  //Gets called when document has loaded
   $.ajax({
     "method" : "GET",
     "url" : "../tinder/api/get-recommendations.php",
     "dataType" : "JSON",
   }).done(function(data){
+    
+    //console.log(data);  //Only for test
     
     //Shuffles the array
     var shuffledArray = data.sort(function() {
@@ -67,4 +71,107 @@ $(document).ready(function() {
   }).fail(function(){
     console.log('{"status" : "error", "message" : "Something went wrong, trying to retrieve the data"}');
   });
+
+  
+  //Keyup on username input field for login
+  $('#input-login-username').keyup(function() {
+    validateUsername();
+  });
+
+  //Keyup on password input field for login
+  $('#input-login-password').keyup(function() {
+    validatePassword();
+  });
+
+  //On submit, call/run the validateLogin()
+  $('#frm-login').submit(function(event) {
+    validateLogin();
+    event.preventDefault();
+  });
+
+  //FUNCTIONS
+  //---------
+  //Function that validates the username
+  function validateUsername() {
+
+    //Regex that checks username: only letters, numbers, dashes and underscores. Between 6 and 16 chars.
+    var usernameRegex = /^[a-zA-Z0-9_-]{5,16}$/;
+
+    if($('#input-login-username').val().match(usernameRegex)) {
+      $('#input-login-username').css('border-color', 'green');
+    } else {
+      $('#input-login-username').css('border-color', 'red');
+    }
+  }
+
+  //Function that validate the password
+  function validatePassword() {
+    
+    //Match 6 to 15 character string with at least one upper case letter, one lower case letter, and one digit
+    var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,15}$/;
+
+    if($('#input-login-password').val().match(passwordRegex)) {
+      $('#input-login-password').css('border-color', 'green');
+    } else {
+      $('#input-login-password').css('border-color', 'red');
+    }
+  }
+
+  //Function that validates the login info all together does the combo of username and password exists?
+  function validateLogin() {
+    
+    var form = $('#frm-login');
+    
+    $.ajax({
+      "method" : "POST",
+      "url" : "../tinder/api/validate-login.php",
+      "data" : form.serialize(),    //Encodes a set of form elements (in this case the form element it self) as a string for submission - What we sent to the API
+      "dataType" : "JSON",    //What type we expect to receive from the API      
+    }).done(function(data) {
+      console.log(data);
+      if(data.status == 'success') {
+        //console.log('YAY');   //Only for test
+
+        swal({
+          title: "Success!",
+          text: "You have successfully been logged in",
+          icon: "success",
+          timer: 2500,
+          buttons: false
+        }).then(function(){
+            //TODO: Redirect to user-profile page
+            //Need to start a session somewhere
+            //Need to bring the userID along aswell somehow
+            window.location.href = '';
+        }); 
+
+      } else if(data.status == 'pending'){
+          //console.log('Activation is required');    //Only for test
+
+          swal({
+            title: "Activation required!",
+            text: "You need to activate your account through the link we sent you in an email during the signup process. ",
+            icon: "info",
+            button: "Roger that!"
+          });
+
+      } else {
+          //console.log('DANG IT');   //Only for test
+
+          //Sweet Alert
+          swal({
+            title: "Woops",
+            text: "Wrong username and/or password!",
+            icon: "error",
+            button: "Try again"
+          });
+
+      }
+    }).fail(function() {
+      console.log('{"status" : "error", "message" : "Some error occured in backend"}');
+    });
+  }
+
+  
 });
+
